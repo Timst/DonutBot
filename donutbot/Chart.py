3512cab44f9d3b75e9a2fef03e33bcba2cff3553
Timst <timst44@gmail.com>
2026-02-07 18:30:52 -0800

    Add labels to non-project mode

--- donutbot/Chart.py
+++ donutbot/Chart.py
@@ -43,10 +43,10 @@ class Chart:
 
         if project:
             plt.figure(figsize=(12, 7))
+            ax = plt.gca()
             max_user = next(iter(self.logic.get_top()))
             max_projection = int(self.logic.get_stats_by_display_name(max_user).projection * 1.25)
 
-            ax = plt.gca()
             ax.set_xlim(start_date, end_date)
             ax.set_ylim(0, max_projection)
             ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
@@ -68,7 +68,7 @@ class Chart:
                     sns.regplot(
                         data=user_data, x="time_numeric", y="cumulative_score",
                         truncate=False, ci=None, scatter=False,
-                        line_kws={"ls": "--", "lw": 1, "alpha": 0.5},
+                        line_kws={"ls": "--", "lw": 1.5, "alpha": 0.5},
                         color=color, ax=ax
                         )
 
@@ -76,33 +76,74 @@ class Chart:
                     final_y = regression.slope * end_date + regression.intercept # type: ignore
 
                     if len(list(filter(lambda area: final_y > area[0] and final_y < area[1], used_label_areas))) > 0:
-                        final_y += 6
+                        final_y += 10
 
                     used_label_areas.append((final_y - 5, final_y + 5))
 
-                    plt.text(
-                        end_date + 2, final_y, user,
-                        color=color, va='center', ha='left', fontsize=9
+                    ax.text(
+                        end_date + 2,
+                        final_y,
+                        user,
+                        color=color,
+                        va='center',
+                        ha='left',
+                        fontsize=9,
+                        fontweight='bold'
                     )
 
         else:
             plt.figure(figsize=(12, 7))
-
-            sns.lineplot(
-                data=data,
-                x="time_numeric",
-                y="cumulative_score",
-                hue="username",
-                linewidth=2,
-                marker='o',
-                markersize=4,
-                errorbar=('ci', False),
-                palette="husl"
-            )
-
             ax = plt.gca()
+
+            daily_data = data.sort_values(['username', 'time']).groupby(
+                ['username', 'time_numeric']
+            ).last().reset_index()
+
+            usernames = sorted(daily_data['username'].unique())
+            palette = sns.color_palette("husl", len(usernames))
+            color_map = dict(zip(usernames, palette))
+
+            used_label_areas = []
+            for user in usernames:
+                user_plot_data = daily_data[daily_data['username'] == user]
+                color = color_map[user]
+
+                ax.plot(
+                    user_plot_data['time_numeric'],
+                    user_plot_data['cumulative_score'],
+                    marker='o',
+                    markersize=4,
+                    linewidth=2,
+                    color=color,
+                    label=user,
+                    linestyle='-'
+                )
+
+                last_row = user_plot_data.iloc[-1]
+                label_x = last_row['time_numeric'] + 0.3
+                label_y = last_row['cumulative_score']
+
+                if (label_x, label_y) in used_label_areas:
+                    label_y += 1
+
+                ax.text(
+                    label_x,
+                    label_y,
+                    user,
+                    color=color,
+                    fontweight='bold',
+                    va='center',
+                    ha='left',
+                    fontsize=9
+                )
+
+                used_label_areas.append((label_x, label_y))
+
             ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
             ax.xaxis.set_major_locator(mdates.AutoDateLocator())
+
+            current_xlim = ax.get_xlim()
+            ax.set_xlim(current_xlim[0], current_xlim[1] + (current_xlim[1] - current_xlim[0]) * 0.15)
 
         plt.xticks(rotation=45)
         plt.xlabel("Date")
@@ -112,11 +153,6 @@ class Chart:
         plt.axvline(today_ordinal, color='black', linestyle=':', alpha=0.5)
         plt.text(today_ordinal, plt.ylim()[1], ' TODAY', rotation=0, va='bottom', fontweight='bold')
         plt.title("DONUTS DONUTS DONUTS")
-        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
         plt.grid(True, alpha=0.2)
-
-        legend = ax.get_legend()
-        if project and legend:
-            legend.remove()
 
         plt.savefig(self.CHART_FILE, dpi=300, bbox_inches='tight')
